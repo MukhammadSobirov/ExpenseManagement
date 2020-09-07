7adec9ea9e19ec78770c29797b2f05b63e7c998a
unknown <madhur.rastogi@primussoft.com>
2020-09-07 19:38:05 +0530

    SignUpAPIComplete

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userControllerRoute = require('../controller/userController');
 
-router.route('/').post(userControllerRoute.signUp);
+router.route('/signUp').post(userControllerRoute.signUp);
 
 module.exports = router;
